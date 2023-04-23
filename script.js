5029175d2dd5abde2d0ec647820cdd494eca7e67
Aleksei_K <kytykob@gmail.com>
2023-04-23 21:57:32 +0300

    modification script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* не безопасный код, т.к. используя eval(), злоумышленнику слишком легко запустить какой угодно код
+/* не безопасный код, т.к. используя eval(), злоумышленнику слишком легко запустить какой угодно код*/
 
 const userInput = document.getElementById('userInput');
 let expression = '';
@@ -14,32 +14,4 @@ const equal = () =>{
 const erase = () =>{
     expression = '';
     userInput.value = expression;
-} */
-
-
-
-
-
-let a = ''; //first number
-let b = ''; //second number
-let sign = '';  //sign operation
-let finish = false;
-
-const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];  // check number
-const action = ['+', '-', '*', '/'];
-
-//https://www.youtube.com/watch?v=Gquaiuj-VpU
-// 4.00
-
-const out = document.getElementById('userInput');
-
-const clearAll = () =>{
-    let a = ''; //first number
-    let b = ''; //second number
-    let sign = '';  //sign operation
-    let finish = false; 
-}
-
-проверь очищается ли поля после чистки 4.45
-
-
+} 
